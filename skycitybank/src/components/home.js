f01b1dbf4ee4859cd7f891c03a18a4651b676946
J Ben Anderson <revovisions@gmail.com>
2021-06-19 21:32:02 -0400

    added styling to home page

--- skycitybank/src/components/home.js
+++ skycitybank/src/components/home.js
@@ -2,21 +2,24 @@ import React from "react";
 import Card from "./card";
 function Home() {
   return (
-    <div className="container">
-      <div className="card align-items-center">
-        <Card
-          txtcolor="white"
-          header="Front-End Bank Landing Page"
-          title="Welcome to the bank"
-          text="You can use this bank"
-          body={
-            <img
-              src="piggyBank.png"
-              className="img-fluid"
-              alt="Responsive image"
-            />
-          }
-        />
+    <div className="card align-items-center">
+      <div className="container">
+        <div className="card align-items-center">
+          <Card
+            txtcolor="white"
+            header="Front-End Bank Landing Page"
+            title="Welcome to the bank"
+            text="You can use this bank"
+            body={
+              <img
+                src="piggyBank.png"
+                className="img-fluid"
+                alt="Responsive image"
+              />
+            }
+          />
+          <div className="sliding-background"></div>
+        </div>
       </div>
     </div>
   );
