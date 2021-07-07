f577a2c8db28f6ef56e485bac0cc6ec2e9b189ce
J Ben Anderson <revovisions@gmail.com>
2021-07-06 21:05:36 -0400

    adding to login page -created logout button, input fields etc.

--- skycitybank/src/components/login.js
+++ skycitybank/src/components/login.js
@@ -7,6 +7,14 @@ function Login() {
 
   const login = JSON.stringify(ctx.users[ctx.users.length - 1].name);
   console.log(login);
+
+  function logout() {
+    let loginForm = document.getElementById("loginForm");
+    ctx.users = [];
+
+    loginForm.className = "visible";
+  }
+
   return (
     <div className="container-fluid">
       <div className="card align-items-center" id="card-align-items-center">
@@ -27,6 +35,54 @@ function Login() {
                 You are logged in as:
                 <h3 id="loginField">{login}</h3>
               </div>
+              <br />
+              <div className="invisible" id="loginForm">
+                <div className="input-group input-group-sm mb-3">
+                  <span className="input-group-text" id="inputGroup-sizing-sm">
+                    login
+                  </span>
+                  <input
+                    type="text"
+                    className="form-control"
+                    aria-label="Sizing example input"
+                    aria-describedby="inputGroup-sizing-sm"
+                    placeholder="email"
+                  ></input>
+                </div>
+
+                <div className="input-group input-group-sm mb-3">
+                  <span className="input-group-text" id="inputGroup-sizing-sm">
+                    password
+                  </span>
+                  <input
+                    type="text"
+                    className="form-control"
+                    aria-label="Sizing example input"
+                    aria-describedby="inputGroup-sizing-sm"
+                    placeholder="password"
+                  ></input>
+                </div>
+
+                <button
+                  type="button"
+                  className="btn btn-light"
+                  id="logoutButton"
+                >
+                  Submit
+                </button>
+              </div>
+              <br />
+              <br />
+
+              <div>Not you? Please</div>
+              <button
+                type="button"
+                className="btn btn-warning"
+                id="logoutButton"
+                onClick={logout}
+              >
+                logout
+              </button>
             </>
           }
         />
