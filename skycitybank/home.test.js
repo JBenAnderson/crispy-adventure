0fd99df33659ffc3a3bd6eb17ed8c039010228ae
J Ben Anderson <revovisions@gmail.com>
2021-09-14 17:09:57 -0400

    changes to npm-test.yml

--- skycitybank/home.test.js
+++ skycitybank/home.test.js
@@ -1,6 +1,6 @@
 const home = require("./src/components/home.js");
 describe("Home container has loaded", () => {
   test("working", () => {
-    expect(home.Home()).toReturnWith(<div></div>);
+    expect(require("../skycitybank/src/components/home.js").Home()).toBeEnabled;
   });
 });
