5f6088749486ecd9ba1cfd8799adf296c7b06e3b
J Ben Anderson <revovisions@gmail.com>
2021-09-14 20:00:12 -0400

    added new test for acceptance testing

--- skycitybank/home.test.js
+++
@@ -1,6 +0,0 @@
-const home = require("./src/components/home.js");
-describe("Home container has loaded", () => {
-  test("working", () => {
-    expect(require("../skycitybank/src/components/home.js").Home()).toBeEnabled;
-  });
-});
